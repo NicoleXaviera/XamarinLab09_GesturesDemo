b970752b9b0647ac1cbff0f72d85f7efac41e829
Alumno <Alumno@LAB1509-23>
2023-11-17 15:37:50 -0500

    Agregar archivos de proyecto.

---
+++ GesturesDemo/GesturesDemo/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace GesturesDemo
+{
+    public partial class App : Application
+    {
+
+        public static float ScreenHeight { get; set;  }
+        public static float Screenwidth { get; set; }
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
